56c7415d7d74c09e5f5de3e4573d87f403753722
Paulo Eduardo Jardim <duhjardim@hotmail.com>
2017-10-22 18:03:05 -0200

    Initial Commit

---
+++ Destroy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destroy
+//
+//  Created by Paulo Eduardo Jardim on 22/10/17.
+//  Copyright © 2017 Paulo Eduardo Jardim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
